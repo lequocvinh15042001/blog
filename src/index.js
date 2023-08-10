9cc7373ce6d69bba3602ee6ae8cee235f7c6003b
Le Quoc Vinh <lequocvinh2001@gmail.com>
2023-08-10 15:27:03 +0200

    c

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express') //import bằng thư viện require
+const path = require('path')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars').engine
+// import { engine } from 'express-handlebars'
+const app = express() //instance của dependence
+const port = 3000; // cổng
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//template engine
+
+app.engine('hbs', handlebars({
+  extname: '.hbs' //sửa lại đuôi .handlebars --> .hbs
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+//
+app.get('/', (req, res) => { //phương thức GET
+  res.render('home');
+})
+
+app.get('/news', (req, res) => { //phương thức GET
+  res.render('news');
+})
+
+//template engines (handlebars): Dùng để tách file,
+// viết file code gọn gàng, không viết dưới dạng string --> Express Handlebars
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
