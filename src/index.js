b3b531d3564ed1f1fd7df82c4bc7f7d10b1c8916
Le Quoc Vinh <lequocvinh2001@gmail.com>
2023-08-14 13:29:26 +0200

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,54 +1,62 @@
-const express = require('express') //import bằng thư viện require
-const path = require('path')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars').engine
+const express = require('express'); //import bằng thư viện require
+const path = require('path');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
 // import { engine } from 'express-handlebars'
-const app = express() //instance của dependence
+const app = express(); //instance của dependence
 const port = 3000; // cổng
 
 //cấu hình file tĩnh img
-app.use(express.static(path.join(__dirname, 'resources/public')))
+app.use(express.static(path.join(__dirname, 'resources/public')));
 
 //dùng middleware
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //template engine
 
-app.engine('hbs', handlebars({
-  extname: '.hbs' //sửa lại đuôi .handlebars --> .hbs
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs', //sửa lại đuôi .handlebars --> .hbs
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //
-app.get('/', (req, res) => { //phương thức GET
-  res.render('home');
-})
+app.get('/', (req, res) => {
+    //phương thức GET
+    res.render('home');
+});
 
-app.get('/news', (req, res) => { //phương thức GET
-  console.log(req.query.q)
-  res.render('news');
-})
+app.get('/news', (req, res) => {
+        //phương thức GET
+        console.log(req.query.q);
+        res.render('news');
+    });
 
-app.post('/search', (req, res) => { //phương thức POST
-  console.log(req.body)
-  res.send('');
-})
+app.post('/search', (req, res) => {
+    //phương thức POST
+          console.log(req.body);
+          res.send('');
+      });
 
-app.get('/search', (req, res) => { //phương thức GET
-  console.log(req.query.q)
-  res.render('search');
-})
-
+app.get('/search', (req, res) => {
+    //phương thức GET
+    console.log(req.query.q);
+    res.render('search');
+});
 
 //template engines (handlebars): Dùng để tách file,
-// viết file code gọn gàng, không viết dưới dạng string --> Express Handlebars
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+        // viết file code gọn gàng, không viết dưới dạng string --> Express Handlebars
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
